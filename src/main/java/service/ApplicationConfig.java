a636a59389b9cb1a29c5ab7cb7d8fa38156a3190
Victor Larsson <larsson.vic@gmail.com>
2016-03-07 16:56:58 +0100

    preparing tests

--- src/main/java/service/ApplicationConfig.java
+++ src/main/java/service/ApplicationConfig.java
@@ -30,6 +30,7 @@ public class ApplicationConfig extends Application {
      */
     private void addRestResourceClasses(Set<Class<?>> resources) {
         resources.add(service.MuseumREST.class);
+        resources.add(service.QuestionREST.class);
         resources.add(service.QuizREST.class);
         resources.add(service.TeamREST.class);
         resources.add(service.VisitorREST.class);
