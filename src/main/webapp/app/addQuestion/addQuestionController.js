06efa7afbf79b83e2d23fffa29b43acbc2314689
Sebastian Sandberg <seb.sandberg@gmail.com>
2016-03-08 18:07:24 +0100

    fixed a submit button

--- src/main/webapp/app/addQuestion/addQuestionController.js
+++ src/main/webapp/app/addQuestion/addQuestionController.js
@@ -1,3 +1,9 @@
 museumApp.controller('addQuestionController',['$scope', function($scope){
     $scope.question = {};
-}]);
+    $scope.submit = function() {
+        //send question here
+        
+        //Empty the question array
+        $scope.question = {};
+    };
+}]);    
--- src/main/webapp/app/services/apiService.js
+++ src/main/webapp/app/services/apiService.js
@@ -2,4 +2,4 @@ var adminService = angular.module('adminService', ['ngResource']);
 
 adminService.factory('adminService', function($resource){
    return $resource('http;//somebackend/:admin', {admin: '@admin'} );
-});
+}); 
