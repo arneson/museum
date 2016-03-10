7bda53d050f80549267f50d48096cc2b13131377
Sebastian Sandberg <seb.sandberg@gmail.com>
2016-03-10 10:55:12 +0100

    login and signup-ish

--- src/main/webapp/app/login/loginController.js
+++ src/main/webapp/app/login/loginController.js
@@ -1,3 +1,18 @@
-museumApp.controller('loginController', function(){
-    
-});
+museumApp.controller('loginController',['$scope','$http', function($scope,$http){
+    $scope.login = function(){
+        var data = {
+            "username": $scope.username,
+            "password": $scope.password
+        }
+        $http({
+            method: 'POST',
+            url: 'http://localhost:8080/Museum/webresources/museum/login',
+            data: data
+        }).then(function successCallback(response){
+            
+            console.log("loginsuccess for: ", response);
+        },  function errorCallback(response) {
+            console.log("loginfail for: " + response);
+        });
+    };
+}]);
