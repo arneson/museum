d5bc9a321e8741ee8f58895da31ca468d107fcaa
Simon Arneson <simon@cymonit.se>
2016-03-13 12:14:48 +0100

    added positonsupport for answers

--- src/main/java/service/QuestionREST.java
+++ src/main/java/service/QuestionREST.java
@@ -176,6 +176,8 @@ public class QuestionREST {
             
             long answer_id  = obj.getJsonNumber("answer_id").longValue();
             long visitor_id = obj.getJsonNumber("visitor_id").longValue();
+            Double posLong = obj.getJsonNumber("longitude").doubleValue();
+            Double posLat = obj.getJsonNumber("latitude").doubleValue();
             Visitor visitor = visitorDAO.find(visitor_id);
             AnswerOption answerOption = answerOptionDAO.find(answer_id);
             boolean result = question.checkAnswer(answerOption);
