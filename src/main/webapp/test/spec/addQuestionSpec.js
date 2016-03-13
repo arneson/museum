d0df7d3d36567959ce4c36d4a8e391031c3f20ea
Sebastian Sandberg <seb.sandberg@gmail.com>
2016-03-13 12:19:46 +0100

    added some test

---
+++ src/main/webapp/test/spec/addQuestionSpec.js
@@ -0,0 +1,29 @@
+/* 
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+describe('addQuestionController', function(){
+    
+    beforeEach(module('museumApp.museumApp'));
+    
+    var $controller;
+    var rootScope;
+    var location;
+    
+    beforeEach(inject(function(_$rootScope_, _$controller_, $location){
+        $controller = _$controller_;
+        rootScope = _$rootScope_;
+        location = $location;
+    }));
+    
+    describe('$scope.init', function() {
+       it('goes back to login screen if no active user', function() {
+           var $scope = {};
+           var controller = $controller('addQuestionController', {$scope: $scope});
+           $scope.init();
+           expect(location.path().toBe('/login'));
+       }); 
+    });
+});
