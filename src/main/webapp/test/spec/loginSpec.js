58e98e17d093cc51a1f1ec8c00ff42c59094883a
Sebastian Sandberg <seb.sandberg@gmail.com>
2016-03-13 23:41:39 +0100

    Register spec

--- src/main/webapp/test/spec/loginSpec.js
+++ src/main/webapp/test/spec/loginSpec.js
@@ -9,14 +9,34 @@ describe('loginController', function(){
     var rootScope, location;
     var location;
     var scope;
+    var mockApiService;
    
     
     beforeEach(module('museumApp'));
+    
+    beforeEach(module(function($provide){
+         mockApiService = {
+               login: jasmine.createSpy()
+           };
+        $provide.value('apiService', mockApiService);
+    }));
     
     beforeEach(inject(function(_$controller_, $location ){
         $controller = _$controller_;
         location = $location; 
     }));
+    describe('Login', function() {
+           beforeEach(inject(function($rootScope){
+            //$scope = {};
+            scope = $rootScope.$new();
+            rootScope = $rootScope;
+            var controller = $controller('loginController', {$scope: scope, $location: location, apiService: mockApiService});
+       }));
+        it('should call apiService.login', function(){
+            scope.login();
+            expect(mockApiService.login).toHaveBeenCalled();
+        });
+    });
     
     describe('With user', function() {
         beforeEach(inject(function($rootScope){
---
+++ src/main/webapp/test/spec/registerSpec.js
@@ -0,0 +1,41 @@
+/* 
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+describe('registerController', function(){
+    var $controller;
+    var rootScope, location;
+    var location;
+    var scope;
+    var mockApiService;
+   
+    
+    beforeEach(module('museumApp'));
+    
+    beforeEach(module(function($provide){
+         mockApiService = {
+               signUp: jasmine.createSpy()
+           };
+        $provide.value('apiService', mockApiService);
+    }));
+    
+    beforeEach(inject(function(_$controller_, $location ){
+        $controller = _$controller_;
+        location = $location; 
+    }));
+    describe('Register', function() {
+           beforeEach(inject(function($rootScope){
+            //$scope = {};
+            scope = $rootScope.$new();
+            rootScope = $rootScope;
+            var controller = $controller('registerController', {$scope: scope, $location: location, apiService: mockApiService});
+       }));
+        it('should call apiService.signUp', function(){
+            scope.register();
+            expect(mockApiService.signUp).toHaveBeenCalled();
+        });
+    });
+    
+});
