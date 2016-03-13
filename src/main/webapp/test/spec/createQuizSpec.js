aa40ba39d542eec23651b33dab4f33dcc008572a
Sebastian Sandberg <seb.sandberg@gmail.com>
2016-03-13 23:56:43 +0100

    added apiService test

--- src/main/webapp/test/spec/createQuizSpec.js
+++ src/main/webapp/test/spec/createQuizSpec.js
@@ -8,9 +8,15 @@ describe('createQuizController', function(){
     var rootScope;
     var location;
     var scope;
-   
+    var mockApiService;
     
     beforeEach(module('museumApp'));
+    beforeEach(module(function($provide){
+         mockApiService = {
+               addQuiz: jasmine.createSpy()
+           };
+        $provide.value('apiService', mockApiService);
+    }));
     
     beforeEach(inject(function(_$controller_, $location ){
         $controller = _$controller_;
@@ -21,17 +27,37 @@ describe('createQuizController', function(){
    
    describe('cancel', function() {
         beforeEach(inject(function($rootScope){
-            //$scope = {};
             scope = $rootScope.$new();
             rootScope = $rootScope;
             rootScope.currentUser = 'someUsr';
             
        }));
         it('Should set location to admin', function() {
-            
             var controller = $controller('createQuizController', {$scope: scope, $location: location});
             scope.cancel();
             expect(location.path()).toBe('/admin');
+        });
+    });
+    
+    describe('submitQuiz', function(){
+        beforeEach(inject(function($rootScope){
+            scope = $rootScope.$new();
+            rootScope = $rootScope;
+            rootScope.currentUser = 'someUsr';
+            scope = {
+                'quiz':{
+                    'name': 'Some quiz',
+                    'points': '20',
+                    'description': 'Some description'
+                }
+            };
+            
+            var controller = $controller('createQuizController', {$scope: scope, $location: location, apiService: mockApiService});
+        }));
+        
+        it('Should call apiService.addQuiz', function(){
+            scope.submitQuiz();
+            expect(mockApiService.addQuiz).toHaveBeenCalled();
         });
     });
 });
